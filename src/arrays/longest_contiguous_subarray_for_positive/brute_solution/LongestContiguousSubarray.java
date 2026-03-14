3b67c3efbd7701f541d5bfd8ab9bebc5742add9b
SwapnilDakhole <swapnildakhole66@gmail.com>
2026-03-14 13:48:51 +0530

    Longest contiguous subarray brute force solution

---
+++ src/arrays/longest_contiguous_subarray_for_positive/brute_solution/LongestContiguousSubarray.java
@@ -0,0 +1,28 @@
+package arrays.longest_contiguous_subarray_for_positive.brute_solution;
+
+public class LongestContiguousSubarray {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 7, 10};
+        int k = 14;
+        int result = longestContiguousSubarray(arr, k);
+        System.out.println(result);
+    }
+
+    private static int longestContiguousSubarray(int[] arr, int k) {
+        if(arr == null || arr.length == 0){
+            throw new IllegalArgumentException("Array can not be null or empty");
+        }
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if(sum > k) break;
+
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+        }
+        return maxLen;
+    }
+}
